eb4bd7267771c8ee095c6f54efdecc1ead47c662
osama mahmoud <osama.mahmoud.h9@gmail.com>
2026-01-10 11:32:17 +0200

    add OpenAPI configuration and update Swagger dependencies; enable API documentation

--- src/main/java/semsem/chatbot/security/SecurityConfig.java
+++ src/main/java/semsem/chatbot/security/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig {
         "/swagger-resources/**",
         "/webjars/**",
         "/configuration/**",
-        "/actuator/**"
+        "/actuator/**",
+
     };
     @Bean
     public PasswordEncoder passwordEncoder() {
